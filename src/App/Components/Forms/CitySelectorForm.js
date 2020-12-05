b40131e470751c866b9d6fbc218a848d2291596f
Sam Mudick <smudick@gmail.com>
2020-12-05 14:14:30 -0600

    added date picker in modal

--- src/App/Components/Forms/CitySelectorForm.js
+++ src/App/Components/Forms/CitySelectorForm.js
@@ -1,24 +1,28 @@
 import React, { Component } from 'react';
+import DateSelector from '../DatePicker';
 
 export default class CitySelectorForm extends Component {
   state = {
     city: '',
-  }
+  };
 
   handleChange = (e) => {
     this.setState({
       [e.target.name]: e.target.value,
     });
-  }
+  };
 
   render() {
     return (
       <div>
-        <select
-          name='city'
-          className='form-control form-control-md'
-          value={this.city}
-          onChange={this.handleChange}>
+        <form className='d-flex flex-column align-items-center'>
+          <p>Select a City</p>
+          <select
+            name='city'
+            className='form-control form-control-md mb-2'
+            value={this.city}
+            onChange={this.handleChange}
+          >
             <option value={'Nashville'}>Nashville</option>
             <option value={'New-York-City'}>New York City</option>
             <option value={'London'}>London</option>
@@ -26,7 +30,11 @@ export default class CitySelectorForm extends Component {
             <option value={'Los-Angeles'}>Los Angeles</option>
             <option value={'Amsterdam'}>Amsterdam</option>
             <option value={'Chicago'}>Chicago</option>
-        </select>
+          </select>
+          <p>Select a Date</p>
+          <DateSelector/>
+          <button className='btn progress-btn mt-3'>Submit</button>
+        </form>
       </div>
     );
   }
