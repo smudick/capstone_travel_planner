448b605df5b9f00534b5a05c60f860210c728dd0
Sam Mudick <smudick@gmail.com>
2020-12-07 19:09:23 -0600

    created layout for activities page

--- src/App/Views/Activities.js
+++ src/App/Views/Activities.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {
+  Button, Form, FormGroup, Input,
+} from 'reactstrap';
 
 export default class Activities extends React.Component {
   render() {
@@ -7,10 +10,44 @@ export default class Activities extends React.Component {
     const displayDate = date.toDateString();
     return (
       <div>
-        <h1>Search for things to do in {city} on {displayDate}</h1>
-        <button className='btn btn-success'>
-            <Link to='/build-itinerary'>Build Itinerary</Link>
-          </button>
+        <div className="d-flex flex-column align-items-center">
+          <h2 className='mb-4 mt-2'>Search for things to do in {city}</h2>
+          <Form inline>
+            <FormGroup className='mb-4 mr-sm-2 mb-sm-0'>
+              <Input
+                type='text'
+                name='activities-search'
+                className='act-search'
+                id='activities-search'
+                placeholder='Example: coffee or Starbucks'
+              />
+            </FormGroup>
+            <Button className='progress-btn'>Submit</Button>
+          </Form>
+          <h3 className='mb-4 mt-4'>Or, if you can't find what you're looking for</h3>
+          <Button className='custom-btn'>
+            Build a custom activity
+          </Button>
+          </div>
+          <div class="container mb-4 mt-4">
+            <div class="row">
+              <div class="col">
+                <h4>
+                Search Results
+                </h4>
+                <div className='search-results'></div>
+              </div>
+              <div class="col">
+                <h4>
+                  Saved Activities
+                </h4>
+                <div className='saved-activities'></div>
+              </div>
+            </div>
+          </div>
+          <Link to='/build-itinerary'>
+            <button className='btn progress-btn mt-2'>Build Itinerary</button>
+          </Link>
       </div>
     );
   }
