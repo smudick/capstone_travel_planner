5fd141e9d2089326a4a359555a960864c7885e03
Sam Mudick <smudick@gmail.com>
2020-12-11 11:07:02 -0600

    layout of itinerary page

--- src/App/Views/ItineraryBuilder.js
+++ src/App/Views/ItineraryBuilder.js
@@ -1,94 +1,79 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {
+  Col,
+  Button,
+  Form,
+  FormGroup,
+  Container,
+  Input,
+  Row,
+  Table,
+} from 'reactstrap';
 
 export default class ItineraryBuilder extends React.Component {
   render() {
+    const timeDiv = [];
+    function itinBuilder() {
+      for (let i = 6; i <= 23; i += 1) {
+        let hour = i;
+        let hourType = 'AM';
+        if (i > 12) {
+          hourType = 'PM';
+          hour = i - 12;
+        } else if (i === 12) {
+          hourType = 'PM';
+        } else {
+          hourType = 'AM';
+          hour = i;
+        }
+        timeDiv.push(
+          <tr>
+            <td className='d-flex justify-content-center'>
+              <h4>
+                {hour} {hourType}
+              </h4>
+            </td>
+            <td className='agenda-events'>
+              <div className='agenda-event'>
+                <Button>Select an Activity</Button>
+              </div>
+            </td>
+          </tr>,
+        );
+      }
+      return timeDiv;
+    }
     return (
-      <div className='container'>
-        <div className='row'>
-          <div className='col'>
-            <h2>Your Adventure is about to begin!</h2>
-            <p>
-              For each time slot, please select one of your saved activites and
-              then save your itinerary
-            </p>
-            <h4>Saved Activities</h4>
-            <div className='saved-activities'>
-              <div className='d-flex flex-wrap justify-content-center'>
-                {this.props.location.props.showCards.showSavedCards()}
-              </div>
+      <Container>
+        <h2>Your Adventure is about to begin!</h2>
+        <p>
+          For each time slot, please select one of your saved activites and then
+          save your itinerary
+        </p>
+        <Col>
+          <div className='agenda'>
+            <div className='table-responsive'>
+              <Table bordered>
+                <thead>
+                  <tr>
+                    <th className='agenda-time'>Time</th>
+                    <th>Activity</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {itinBuilder()}
+                </tbody>
+              </Table>
             </div>
-            <button className='btn btn-success'>
-              <Link to='/activities'>Add More Activities</Link>
-            </button>
           </div>
-          <div className='col'>
-            <div className='agenda'>
-              <div className='table-responsive'>
-                <table className='table table-condensed table-bordered'>
-                  <thead>
-                    <tr>
-                      <th>Date</th>
-                      <th>Time</th>
-                      <th>Event</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td
-                        className='agenda-date'
-                        rowspan='1'
-                      >
-                        <div className='dayofmonth'>26</div>
-                        <div className='dayofweek'>Saturday</div>
-                        <div className='shortdate text-muted'>July, 2014</div>
-                      </td>
-                      <td className='agenda-time'>5:30 AM</td>
-                      <td className='agenda-events'>
-                        <div className='agenda-event'>
-                          <i
-                            className='glyphicon glyphicon-repeat text-muted'
-                            title='Repeating event'
-                          ></i>
-                            Fishing
-                        </div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td className='agenda-date' rowspan='3'>
-                        <div className='dayofmonth'>24</div>
-                        <div className='dayofweek'>Thursday</div>
-                        <div className='shortdate text-muted'>July, 2014</div>
-                      </td>
-                      <td className='agenda-time'>8:00 - 9:00 AM</td>
-                      <td className='agenda-events'>
-                        <div className='agenda-event'>Doctor's Appointment</div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td className='agenda-time'>10:15 AM - 12:00 PM</td>
-                      <td className='agenda-events'>
-                        <div className='agenda-event'>
-                          Meeting with executives
-                        </div>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td className='agenda-time'>7:00 - 9:00 PM</td>
-                      <td className='agenda-events'>
-                        <div className='agenda-event'>Aria's dance recital</div>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            </div>
-            <button className='btn btn-secondary'>
-              <Link to='/saved-itineraries'>Save Your Itinerary</Link>
+          <Link to='/saved-itineraries'>
+            <button className='btn btn-secondary save-btn'>
+              Save Your Itinerary
             </button>
-          </div>
-        </div>
-      </div>
+          </Link>
+        </Col>
+      </Container>
     );
   }
 }
